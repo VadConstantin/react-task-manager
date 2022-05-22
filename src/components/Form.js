c83a3358688ea95a1d0df31c2e06601bd44125e3
Vadim Constantin <vad.constantin@gmail.com>
2022-05-22 15:32:42 +0200

    fixed the React uncontrolled component input error

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,20 +20,20 @@ const Form = () => {
       <h1>My form</h1>
       <div>
         <label htmlFor="prenom"> Prénom</label>
-        <input type="text" id="prenom" name="prenom" onChange={handleChange} value={profile.prenom}/>
+        <input type="text" id="prenom" name="prenom" onChange={handleChange} value={profile.prenom || ''}/>
       </div>
       <div>
         <label htmlFor="nom"> Nom</label>
-        <input type="text" id="nom" name="nom" onChange={handleChange} value={profile.nom}/>
+        <input type="text" id="nom" name="nom" onChange={handleChange} value={profile.nom || ''}/>
       </div>
       <div>
         <label htmlFor="newsletter"> Newsletter</label>
-        <input type="checkbox" id="newsletter" name="neswletter" onChange={handleChange} value={profile.newsletter}/>
+        <input type="checkbox" id="newsletter" name="neswletter" onChange={handleChange} value={profile.newsletter || ''}/>
       </div>
 
       <ul style={{marginTop: 20}}>
         {Object.keys(profile).map((key) => {
-          return <li><strong>{key}:</strong> {profile[key] === true || false ? String(profile[key]) : profile[key]}</li>
+          return <li key={key}><strong>{key}:</strong> {profile[key] === true || false ? String(profile[key]) : profile[key]}</li>
         })}
       </ul>
     </div>
